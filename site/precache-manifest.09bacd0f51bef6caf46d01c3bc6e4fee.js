dd5c7be395ca60e1baee0babb9574e2c40ce3235
Michał Szczygieł <1153719+mszczygiel@users.noreply.github.com>
2020-06-21 22:25:22 +0200

    add site

---
+++ site/precache-manifest.09bacd0f51bef6caf46d01c3bc6e4fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e972f87b47c42dcdcdc61ec6515aed2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67b0fe53bc278297c91",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "60a92b6b34b77f690365",
+    "url": "/static/js/2.6b25ba00.chunk.js"
+  },
+  {
+    "revision": "1b416b749e0907f1fd386338d51f2441",
+    "url": "/static/js/2.6b25ba00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67b0fe53bc278297c91",
+    "url": "/static/js/main.9fb802ea.chunk.js"
+  },
+  {
+    "revision": "3abbe1a2d93c3dffea62",
+    "url": "/static/js/runtime-main.922a85fb.js"
+  }
+]);
